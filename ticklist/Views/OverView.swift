cfc597cf86185f72b90e8d058b37c95d9a887462
Torbjørn Wiik <torwii22@gmail.com>
2024-09-05 21:00:39 +0200

    feat: add and delete ticks

--- ticklist/Views/OverView.swift
+++ ticklist/Views/OverView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct OverView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @Query private var ticks: [Tick]
     
     private let searchbarPrompt = "Silence"
     
@@ -19,23 +23,45 @@ struct OverView: View {
             ZStack {
                 VStack{
                     List {
-                        ForEach(0..<10) { _ in
-                            TickCardView()
+                        ForEach(ticks) { tick in
+                            NavigationLink{
+                                Text ("Placeholder") //TODO: update
+                            } label: {
+                                TickCardView()
+                            }
                         }
+                        .onDelete(perform: deleteTick)
                     }
                     .searchable(text: $text, prompt: searchbarPrompt)
                 }
                 VStack {
                     Spacer()
-                    AddTickButtonView(action: {})
+                    AddTickButtonView(action: addTick)
                         .background(Color.white)
                 }
             }
         }
         .navigationTitle("Ticklist")
     }
+    
+    
+    private func addTick() -> Void {
+        withAnimation{
+            let newTick = Tick(climbName: "foo", cragName: "bar", dateClimbed: .now, grade: "ba", comment: "baba")
+            modelContext.insert(newTick)
+        }
+    }
+
+    private func deleteTick(offsets: IndexSet) -> Void {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(ticks[index])
+            }
+        }
+    }
 }
 
 #Preview {
     OverView(text: "")
+        .modelContainer(for: Tick.self, inMemory: true)
 }
