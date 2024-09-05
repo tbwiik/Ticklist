4b36f1b21951a07036c8edfc3503b6aea98b5ac4
Torbjørn Wiik <torwii22@gmail.com>
2024-09-05 19:56:51 +0200

    ui: create card prototype

---
+++ ticklist/Views/TickCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TickCardView.swift
+//  ticklist
+//
+//  Created by Torbjørn Wiik on 05/09/2024.
+//
+
+import SwiftUI
+
+struct TickCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TickCardView()
+}
