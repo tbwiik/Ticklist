9f057760077a9bb914ec2edd86e061bb4387ade4
Torbjørn Wiik <torwii22@gmail.com>
2024-09-05 20:47:52 +0200

    feat: model tick

--- ticklist/Models/Tick.swift
+++ ticklist/Models/Tick.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Tick: Identifiable {
+    
+    var id: UUID
+    var climbName: String
+    var cragName: String
+    var dateClimbed: Date
+    var grade: String // TODO: update
+    var comment: String
+    
+    
+    init (climbName: String, cragName: String, dateClimbed: Date, grade: String, comment: String) {
+        self.id = UUID()
+        self.climbName = climbName
+        self.cragName = cragName
+        self.dateClimbed = dateClimbed
+        self.grade = grade
+        self.comment = comment
+    }
+}
