fe75837de7260b164e80db49faf8219b72de7e38
Torbjørn Wiik <torwii22@gmail.com>
2024-09-14 10:02:50 +0200

    fix: only use french alternative for grading

---
+++ ticklist/Views/GradePickerView.swift
@@ -0,0 +1,34 @@
+//
+//  GradePickerView.swift
+//  ticklist
+//
+//  Created by Torbjørn Wiik on 13/09/2024.
+//
+
+import SwiftUI
+
+struct GradePickerView: View {
+    
+    @Binding var grade: Grade
+    
+    var body: some View {
+        HStack{
+            Text("Grade")
+            Spacer()            
+            Picker("Grade of Climb", selection: $grade.value) {
+                ForEach(FrenchClimbingGrades.allCases){ grade in
+                    Text(grade.rawValue).tag(grade)
+                }
+            }
+            .pickerStyle(.menu)
+            .background(Color(UIColor.systemGray5))
+            .tint(.black)
+            .cornerRadius(8)
+            .padding(8)
+        }
+    }
+}
+
+#Preview {
+    GradePickerView(grade: .constant(Grade(FrenchClimbingGrades.grade6a)))
+}
