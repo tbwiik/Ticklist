be94db3fad41760181e94a004070c389102d877b
Torbjørn Wiik <torwii22@gmail.com>
2024-09-28 10:17:55 +0200

    feat: make disabled version of picker for type of climb

--- ticklist/Utils/TypeOfClimbPicker.swift
+++ ticklist/Utils/TypeOfClimbPicker.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct TypeOfClimbPicker: View {
     //MARK: - Properties
+    @Environment(\.isEnabled) private var isEnabled
     @Binding var typeOfClimb: TypeOfClimb
     
     //MARK: - View Body
+    var body: some View {
+        if isEnabled {
+            TypeOfClimbPickerEnabled(typeOfClimb: $typeOfClimb)
+        } else {
+            TypeOfClimbPickerDisabled(typeOfClimb: typeOfClimb)
+        }
+    }
+}
+
+private struct TypeOfClimbPickerEnabled: View {
+    @Binding var typeOfClimb: TypeOfClimb
+   
     var body: some View {
         Picker("Type of Climb", selection: $typeOfClimb) {
             ForEach(TypeOfClimb.allCases) { type in
@@ -22,8 +35,17 @@ struct TypeOfClimbPicker: View {
     }
 }
 
+private struct TypeOfClimbPickerDisabled: View {
+    let typeOfClimb: TypeOfClimb
+    
+    var body: some View {
+        Text(typeOfClimb.description)
+    }
+}
+
 #Preview {
     Form {
         TypeOfClimbPicker(typeOfClimb: .constant(TypeOfClimb.defaultValue))
+            .disabled(false)
     }
 }
